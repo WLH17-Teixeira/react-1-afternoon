548a5f3ab22361200ceb7c31ca106f86a2373fca
andre <andrelaprov@gmail.com>
2020-04-09 09:55:43 -0600

    review. finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React, { Component } from 'react';
-
-class App extends Component {
+import React, { Component } from "react";
+import "./index.css";
+import TopicBrowser from "./components/TopicBrowser/TopicBrowser";
+// import Practice from "./components/tests/Practice";
+// import Practice2 from "./components/tests/Practice2";
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: ""
+    };
+  }
+  updateName = word => {
+    this.setState({
+      name: word
+    });
+  };
   render() {
-    
+    return (
+      <div>
+        {/* <Practice name={"Matt"} updateName={this.updateName} />
+        <Practice name={"Andre"} updateName={this.updateName} />
+        {this.state.name}
+        <Practice2 /> */}
+
+        <TopicBrowser />
+      </div>
+    );
   }
 }
-
-export default App;
